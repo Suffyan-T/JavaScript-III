f553a0666e92181a4dfe201b86df5c9eda8b0222
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-07-24 16:01:22 -0700

    Started working on this file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Object Binding:
+        When the .this is not inside an object it is in the global object.
+* 2.  Implicit Binding:
+        when you call a function/method inside an object whatever comes before the dot is the this object.
+* 3. New binding:
+        when using a Constructor the object that is created by the constructor is the this.
+* 4. Explicit binding:
+        You can use .call or .apply to bind this to an object of your choosing
 *
 * write out a code example of each explanation above
 */
@@ -12,10 +16,14 @@
 // Principle 1
 
 // code example for Window Binding
-
+function principal1() {
+    // The this below is refering to global object
+    console.log(this);
+  }
 // Principle 2
 
 // code example for Implicit Binding
+
 
 // Principle 3
 
